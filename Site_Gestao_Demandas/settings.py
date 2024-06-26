8378bbc4c5640793e422ffdfe4cd57c7c8c38a5c
HudsonSMadeira <129692594+HudsonSMadeira@users.noreply.github.com>
2024-06-26 00:36:15 -0300

    atualização

--- Site_Gestao_Demandas/settings.py
+++ Site_Gestao_Demandas/settings.py
@@ -68,7 +68,6 @@ WSGI_APPLICATION = 'Site_Gestao_Demandas.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gestaodemandas_db',
